2b72b1da470974f132bb9c36d95004a3f9407a31
Roger Hansen <xwhitemousex@hotmail.com>
2017-05-04 21:47:52 +0200

    Update dice_sim.py

--- dice_sim.py
+++ dice_sim.py
@@ -9,7 +9,7 @@ dice = 0;
 i = 0;
 attr = {'Str': 1, 'Sta': 1, 'Con': 1, 'Int': 1, 'Wis': 1, 'Cha': 1};
 
-#attributes
+#stats_roll called to roll random value to be assigned to dictionary keys
 def stats_roll(attribute):
 	result = 0
 	for x in range(3):
@@ -19,10 +19,10 @@ def stats_roll(attribute):
 
 def addStats(attributes):
 	for keys in attr:
-		attr.values() = stats_roll(attrib)
+		attr.values() = stats_roll(attrib) #does not work...
 	print (attr.items())
 
-
+#how to assign random rolled values to each of the keys in the dictinary???
 for value in attr:
 	print (value), attr[value]
 

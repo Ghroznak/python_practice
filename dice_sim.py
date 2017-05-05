a8168bc838dcd0a029a4310d5d2a5109ae3a567a
Roger Hansen <xwhitemousex@hotmail.com>
2017-05-05 09:26:53 +0200

    Updated with functioning dictionary
    Assigned attributes and attribute_values in two separate lists then zipped together into a dictionary after values have been generated.

--- dice_sim.py
+++ dice_sim.py
@@ -3,28 +3,28 @@ import random
 
 #init
 n = 3; #number of dice
-d = 6; #type of dice (d6, d8 etc)
+d = 6; #numbers on dice
 result = 0;
 dice = 0;
-i = 0;
-attr = {'Str': 1, 'Sta': 1, 'Con': 1, 'Int': 1, 'Wis': 1, 'Cha': 1};
+
+attributes = ('Str', 'Sta', 'Con', 'Int', 'Wis', 'Cha');
 
 #stats_roll called to roll random value to be assigned to dictionary keys
 def stats_roll(attribute):
 	result = 0
-	for x in range(3):
-		dice = random.randint(1, 6)
+	for x in range(n):
+		dice = random.randint(1, d)
 		result = result + dice
 	return result
 
-def addStats(attributes):
-	for keys in attr:
-		attr.values() = stats_roll(attrib) #does not work...
-	print (attr.items())
+#assigning values to list
+attribute_values = (stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int))
 
-#how to assign random rolled values to each of the keys in the dictinary???
-for value in attr:
-	print (value), attr[value]
+#zipping attributes and attribute_values together into a dictionary
+character_attributes = dict(zip(attributes, attribute_values)) 
+
+
+print (character_attributes.items())
 
 
 

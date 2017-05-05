b5872b8d0438e6c444b9e1f060ab002aca5fa08c
Roger Hansen <xwhitemousex@hotmail.com>
2017-05-05 17:03:57 +0200

    Update dice_sim.py

--- dice_sim.py
+++ dice_sim.py
@@ -1,4 +1,4 @@
-#dice rolling simulator
+#mini dungeon game
 import random
 import time
 from time import sleep
@@ -24,17 +24,18 @@ def health(int):	#determines health.
 
 #assigning values to list
 hero_attrib_values = (stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int))
-monster_attrib_values = (stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int))
 
 #zipping attributes and attribute_values together into a dictionary
-
 hero_attributes = dict(zip(attributes, hero_attrib_values)) 
-monster_attributes = dict(zip(attributes, monster_attrib_values))
-
 hero_attributes['HP'] = health(hero_attributes['Con'])
-monster_attributes['HP'] = health(monster_attributes['Con'])
 
-def attack_roll(atk, dfs):
+def create_monster(dict):
+	monster_attrib_values = (stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int), stats_roll(int))
+	monster_attributes = dict(zip(attributes, monster_attrib_values))
+	monster_attributes['HP'] = health(monster_attributes['Con'])
+	return monster_attributes
+
+def attack_roll(atk, dfs):				#determines if it is a hit or miss
 	attacker = atk * random.randint(1,20)
 	defender = dfs * random.randint(1,20)
 	if attacker > defender:
@@ -42,33 +43,48 @@ def attack_roll(atk, dfs):
 	else:
 		return False
 
-def damage_roll(int):
+def damage_roll(int):				#determines amount of damage done.
 	result = int + random.randint(1, 6)
 	return result
 	
 def initiative(int):
 	result = random.randint(1,20)
 	return result
-	
-while hero_attributes['HP']>0 or monster_attributes['HP']>0:
-	if attack_roll(hero_attributes['Str'], monster_attributes['Agi']) == True:
-		monster_attributes['HP'] -= damage_roll(hero_attributes['Str'])
-		print ("Donald Trump is down to " + str(monster_attributes['HP']) + " hitpoints!")
-	else:
-		print ("Hero missed!")
-	time.sleep(2)
-	if monster_attributes['HP'] < 1:
-		break
-	if attack_roll(monster_attributes['Str'], hero_attributes['Agi']) == True:
-		hero_attributes['HP'] -= damage_roll(monster_attributes['Str'])
-		print ("Hero is down to " + str(hero_attributes['HP']) + " hitpoints!")
-	else:
-		print ("Monster missed!")
-	time.sleep(2)
-	if hero_attributes['HP'] < 1:
-		break
 
-if monster_attributes['HP']<hero_attributes['HP']:
-	print ("Hero has won")
-else:
-	print ("Monster has won")
+def encounter():
+	monster_attributes = create_monster(dict)
+	print (monster_attributes.items())
+	print (hero_attributes.items())
+	while hero_attributes['HP']>0 or monster_attributes['HP']>0:
+		if attack_roll(hero_attributes['Str'], monster_attributes['Agi']) == True:
+			dmg = damage_roll(hero_attributes['Str'])
+			monster_attributes['HP'] -= dmg
+			print ("Monster was hit for " + str(dmg) + " damage and is down to " + str(monster_attributes['HP']) + " hitpoints!")
+		else:
+			print ("Hero missed!")
+		time.sleep(2)
+		if monster_attributes['HP'] < 1:
+			break
+		if attack_roll(monster_attributes['Str'], hero_attributes['Agi']) == True:
+			dmg = damage_roll(monster_attributes['Str'])
+			hero_attributes['HP'] -= dmg
+			print ("Hero was hit for " + str(dmg) + " damage and is down to " + str(hero_attributes['HP']) + " hitpoints!")
+		else:
+			print ("Monster missed!")
+		time.sleep(2)
+		if hero_attributes['HP'] < 1:
+			break
+
+	if monster_attributes['HP'] < hero_attributes['HP']:
+		print ("Hero has won")
+		healing = 5 * random.randint(1, 10)
+		hero_attributes['HP'] += healing
+		if hero_attributes['HP'] > hero_attributes['Con'] * 5:
+			hero_attributes['HP'] = hero_attributes['Con'] * 5
+		print ("Hero consumed a healing potion and healed up for " + str(healing) + " points. Lets kill another monster!")
+		time.sleep(2)
+	else:
+		print ("Monster has won")
+	
+while hero_attributes['HP'] > 0:
+	encounter()

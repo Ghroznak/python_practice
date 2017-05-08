ae3ab76eb05c82bd1be113a4f4db87fd68a718e7
Roger Hansen <xwhitemousex@hotmail.com>
2017-05-08 13:24:57 +0200

    Moved some "loose" code into def's

--- lottery-en.py
+++ lottery-en.py
@@ -10,7 +10,7 @@ i = 0;
 winner = [[], [], [], [], [], [], []]
 
 
-def lottonumbers(list):			#generates random lotto numbers
+def lottonumbers(*args):			#generates random lotto numbers
 	number = random.sample(range(1,35), 7)
 	number.sort()
 	return number
@@ -23,27 +23,27 @@ def compare(win, lotto):			#compares the player numbers with the randomly genera
 		n += 1
 	return False
 
-#generates 5 random lotto numbers
-n = 0;
-for list in winner:
-	winner[n] = lottonumbers(dict)
-	n += 1
+def player_numbers(*args):		#generates 5 random lotto numbers
+	n = 0;
+	for list in winner:
+		winner[n] = lottonumbers(dict)
+		n += 1
+	return winner
 
 while i < 50:					#randomly draws new numbers until winning 50 times.
 	start = t.time()
-	print ("These are the golden tickets: " + str(winner))
 	while not win:
-		lottono = random.sample(range(1,35), 7)
-		lottono.sort()
+		lottono = lottonumbers()
+		winner = player_numbers()
 		win = compare(winner, lottono)
-		rows += 5	#rows played each time lottery runs
-		cost = rows * 5 #Norwegian Crowns per row played 
+		rows += 5
+		cost = rows * 5 
 	
 	#writes results to file with comma seperation
 	#f = open('H:\Delete Me\lotto.txt', 'a')
 	#f.write("# of rows," + str(rows) + "," + "Cost," + str(cost) + "," + "Winning number was" + "," + str(winner) + '\n')
 	
-	print("This time it cost: " + str(cost)) #prints out total cost in NOK to achieve a lottery win
+	print("This time it cost: " + str(cost) + "\nWinning number was " + str(lottono)) #prints out total cost in NOK to achieve a lottery win
 	
 	#resets values for the next run of the loop
 	win = False;

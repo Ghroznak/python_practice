75bc848ce962864a435014aadc96f365e89b25ef
Roger Hansen <xwhitemousex@hotmail.com>
2017-05-10 14:20:41 +0200

    Update lottery-en.py

--- lottery-en.py
+++ lottery-en.py
@@ -9,19 +9,10 @@ win = False;
 i = 0;
 winner = [[], [], [], [], [], [], []]
 
-
 def lottonumbers(*args):			#generates random lotto numbers
 	number = random.sample(range(1,35), 7)
 	number.sort()
 	return number
-
-def compare(win, lotto):			#compares the player numbers with the randomly generated number
-	n = 0;
-	for lst in win:
-		if lotto == win[n]:
-			return True
-		n += 1
-	return False
 
 def player_numbers(*args):		#generates 5 random lotto numbers
 	n = 0;
@@ -35,15 +26,15 @@ while i < 50:					#randomly draws new numbers until winning 50 times.
 	while not win:
 		lottono = lottonumbers()
 		winner = player_numbers()
-		win = compare(winner, lottono)
-		rows += 5
-		cost = rows * 5 
+		win = lottono in winner
+		rows += 1
+		cost = rows * 25 
 	
 	#writes results to file with comma seperation
 	#f = open('H:\Delete Me\lotto.txt', 'a')
 	#f.write("# of rows," + str(rows) + "," + "Cost," + str(cost) + "," + "Winning number was" + "," + str(winner) + '\n')
 	
-	print("This time it cost: " + str(cost) + "\nWinning number was " + str(lottono)) #prints out total cost in NOK to achieve a lottery win
+	print("Rows generated: " + str(rows) + "\nThis time it cost: " + str(cost) + "\nWinning number was " + str(lottono)) #prints out total cost in NOK to achieve a lottery win
 	
 	#resets values for the next run of the loop
 	win = False;
